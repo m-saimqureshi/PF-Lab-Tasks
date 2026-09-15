dfd5b99bfa650b7242064f3b4e4d51b85d1e4ab4
m-saimqureshi <324948200+m-saimqureshi@users.noreply.github.com>
2026-09-15 23:07:02 +0500

    Add scholarship eligibility check in Task1.c

--- Post Lab Tasks/Lab-04 /Task1.c
+++ Post Lab Tasks/Lab-04 /Task1.c
@@ -1,1 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
 
+int main() {
+    float p, a;
+    
+    printf("Enter percentage: ");
+    scanf("%f", &p);
+    printf("Family income: ");
+    scanf("%f", &a);
+    
+    if (p>=80 || a<=50000) {
+        printf("\nStudent is eligible for the scholarship");
+    }
+    else
+        printf("\nStudent is ineligible for the scholarship");
+        
+    return 0;
+}
