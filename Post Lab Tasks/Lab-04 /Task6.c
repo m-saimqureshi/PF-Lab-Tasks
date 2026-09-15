951a76d35116a07949e18bd64445d2b5eea94d70
m-saimqureshi <324948200+m-saimqureshi@users.noreply.github.com>
2026-09-15 23:19:10 +0500

    Add Task6.c for Celsius to Fahrenheit Converter

---
+++ Post Lab Tasks/Lab-04 /Task6.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main(){
+    int C,F;
+    printf("Temperature in °c = ");
+    scanf("%d", &C);
+    F=(C*9/5)+32;
+    printf("\nTemperature in °F = %d",F);
+    return 0;
+}
