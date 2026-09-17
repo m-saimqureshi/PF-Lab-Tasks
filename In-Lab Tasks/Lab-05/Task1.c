5ee6ce07156917815adfa2da70ca5cc513506ff9
m-saimqureshi <324948200+m-saimqureshi@users.noreply.github.com>
2026-09-17 20:55:48 +0500

    Implement admission eligibility check
    Added eligibility check for admission based on age and marks.

--- In-Lab Tasks/Lab-05/Task1.c
+++ In-Lab Tasks/Lab-05/Task1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+ int age, marks;
+    printf("Enter student age: ");
+    scanf("%d", &age);
+     printf("Enter student marks: ");
+    scanf("%d", &marks);
+    if (age>=18) {
+        if (marks>=50) {
+            printf("\nEligible for the admission");
+        }
+        else 
+        printf("\nAdmission criteria not fulfilled");
+    }
+    
+    return 0;
+}
