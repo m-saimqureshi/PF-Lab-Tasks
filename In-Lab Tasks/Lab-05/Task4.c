e533644f45503e424d561c72df2314386d4fe41b
m-saimqureshi <324948200+m-saimqureshi@users.noreply.github.com>
2026-09-17 21:00:03 +0500

    grading program to check pass/fail status
    Implement a simple grading program that checks if marks are passing or failing based on a threshold.

---
+++ In-Lab Tasks/Lab-05/Task4.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float marks;
+ printf("Enter marks: ");
+    scanf("%.2f", &marks);
+    printf(marks>=50? "Pass" : "Fail" );
+    
+    return 0;
+}
