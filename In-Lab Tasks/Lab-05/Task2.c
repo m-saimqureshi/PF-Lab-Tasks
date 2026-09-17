0e8f4f5e2681927038865d18b27c3f1c2b51bc84
m-saimqureshi <324948200+m-saimqureshi@users.noreply.github.com>
2026-09-17 20:56:57 +0500

    Implement eligibility check for driving license
    Added user input prompts for CNIC and driving test eligibility.

--- In-Lab Tasks/Lab-05/Task2.c
+++ In-Lab Tasks/Lab-05/Task2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int a,b;
+    printf("(NOTE: YES=1 and NO=0)\n");
+    printf("Do you have a CNIC? ");
+    printf("\nUser: ");
+    scanf("%d",&a);
+    printf("Did you pass the driving test?");
+    printf("\nUser: ");
+    scanf("%d", &b);
+    if (a==1) {
+        if (b==1){
+            printf("You are eligible for DRIVING LICENSE.");
+        }
+        else 
+            printf("You are not eligible for DRIVING LICENSE.");
+    }
+    return 0;
+}
